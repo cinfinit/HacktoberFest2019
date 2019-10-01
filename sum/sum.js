07364d32e0debc99c6c0a0eade6a8a01a7b5aa19
cinfinit <39793609+cinfinit@users.noreply.github.com>
2019-10-01 12:00:23 +0530

    added sum program in JS

---
+++ sum/sum.js
@@ -0,0 +1,16 @@
+// function that calculates the sum of 2 number
+//you can add more parameter depending upon your requirement
+
+//naive way to make function
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// arrow function
+const sum = (a, b) => {
+  return a + b;
+};
+let a = 5;
+let b = 6;
+let c = sum(a, b);
+console.log(c);
